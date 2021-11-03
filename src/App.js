9151ea37fc4a50472cd68d32e6a696f532c21718
DomBarbosa8 <mobarbosa.phoenix@gmail.com>
2021-11-02 22:56:23 -0300

    primeiroReact

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import homemFeliz from './assets/homem-feliz-dinheiro.jpg';
+
+function App() {
+  return (
+    <div className="App-header">
+    <img src={homemFeliz} className="App-logo" alt="homem feliz" />
+    <h1>\o/</h1>
+    <h2>Não está 100% certo, mas também não está errado!</h2>
+    </div>
+    );
+}
+
+export default App;
